73201a423e90e9f675e210a830917ba7b6c20df4
Duy K. Bui <duy@buifamily.info>
2019-09-21 15:23:17 -0400

    feat: Core functionality of home page

---
+++ app/App.svelte
@@ -0,0 +1,133 @@
+<script>
+  import {
+    categoryLabels,
+    categoryIndex,
+    price,
+    taxes,
+    taxIndex,
+    taxLabel,
+    volumes,
+    volumeIndex,
+    volumeLabel,
+    sizes,
+    sizeIndex,
+    sizeLabel,
+    weight,
+    final
+  } from "./data";
+
+  const print = () => {};
+</script>
+
+<page class="page">
+  <actionBar class="action-bar" title="Tính Giá Nhanh" icon="">
+    <!-- <actionItem on:tap={print} android.systemIcon="ic_menu_camera" /> -->
+  </actionBar>
+  <scrollView class="body">
+    <stackLayout class="form">
+      <segmentedBar bind:selectedIndex={$categoryIndex}>
+        <segmentedBarItem title="Nước hoa" />
+        <segmentedBarItem title="Mỹ phẩm" />
+        <segmentedBarItem title="Son" />
+        <segmentedBarItem title="Khác" />
+      </segmentedBar>
+
+      <gridLayout
+        class="input-field input-sides"
+        rows="auto auto"
+        columns="60 * 75">
+        <label row="0" col="0" text="Giá" />
+        <stackLayout
+          class="input input-border"
+          row="0"
+          col="1"
+          margin="0 10"
+          padding="0 5">
+          <textField class="input" bind:text={$price} keyboardType="number" />
+        </stackLayout>
+        <label row="0" col="2" text="USD" />
+      </gridLayout>
+
+      <gridLayout
+        class="input-field input-sides"
+        rows="auto auto"
+        columns="60 * 75">
+        <label row="0" col="0" text="Thuế" />
+        <slider
+          row="0"
+          col="1"
+          bind:value={$taxIndex}
+          minValue="0"
+          maxValue={taxes.length - 1} />
+        <label row="0" col="2" text={$taxLabel} textWrap="true" />
+      </gridLayout>
+
+      <gridLayout
+        class="input-field input-sides"
+        rows="auto auto"
+        columns="60 * 75">
+        <label row="0" col="0" text="Cỡ" />
+        <!-- Perfume -->
+        <slider
+          row="0"
+          col="1"
+          bind:value={$volumeIndex}
+          minValue="0"
+          maxValue={volumes.length - 1}
+          visibility={$categoryIndex === 0 ? '' : 'collapsed'} />
+        <label
+          row="0"
+          col="2"
+          text={$volumeLabel}
+          visibility={$categoryIndex === 0 ? '' : 'collapsed'}
+          textWrap="true" />
+        <!-- Cosmetics -->
+        <slider
+          row="0"
+          col="1"
+          bind:value={$sizeIndex}
+          minValue="0"
+          visibility={$categoryIndex === 1 ? '' : 'collapsed'}
+          maxValue={sizes.length - 1} />
+        <label
+          row="0"
+          col="2"
+          text={$sizeLabel}
+          visibility={$categoryIndex === 1 ? '' : 'collapsed'}
+          textWrap="true" />
+        <!-- Lipsticks -->
+        <label
+          row="0"
+          col="1"
+          text="4"
+          margin="0 10"
+          padding="0 5"
+          visibility={$categoryIndex === 2 ? '' : 'collapsed'} />
+        <label
+          row="0"
+          col="2"
+          text="oz"
+          visibility={$categoryIndex === 2 ? '' : 'collapsed'}
+          textWrap="true" />
+        <!-- Others -->
+        <stackLayout
+          class="input input-border"
+          row="0"
+          col="1"
+          margin="0 10"
+          padding="0 5"
+          visibility={$categoryIndex > 2 ? '' : 'collapsed'}>
+          <textField class="input" bind:text={$weight} keyboardType="number" />
+        </stackLayout>
+        <label
+          row="0"
+          col="2"
+          text="oz"
+          visibility={$categoryIndex > 2 ? '' : 'collapsed'}
+          textWrap="true" />
+      </gridLayout>
+
+      <label class="h1 text-center" text={$final} />
+    </stackLayout>
+  </scrollView>
+</page>
---
+++ app/data.ts
@@ -0,0 +1,88 @@
+import { readable, writable, derived } from 'svelte/store';
+
+const usdToVnd = 23500;
+const shippingCost = 5; // $/lb
+const lipstickWeight = 0.25; // lb
+const perfumeCustom = 5; // $/item
+const cosmeticsCustom = 2.5; // $/lb
+const lipstickCustom = (priceWithTax: number) => {
+  if (priceWithTax >= 30) return 1.5;
+  return 0.5;
+}; // $/item
+
+export const categories = ['perfume', 'cosmetics', 'lipsticks', 'others'];
+export const categoryLabels = ['Nước hoa', 'Mỹ phẩm', 'Son', 'Khác'];
+export const categoryIndex = writable(0);
+export const category = derived(categoryIndex, from(categories));
+export const categoryLabel = derived(categoryIndex, from(categoryLabels));
+
+export const price = writable(0);
+
+export const taxes = [1, 1.05, 1.06, 1.075, 1.1];
+export const taxLabels = ['0%', '5%', '6%', '7.5%', '10%'];
+export const taxIndex = writable(2);
+export const tax = derived(taxIndex, (taxIndex) => taxes[taxIndex]);
+export const taxLabel = derived(taxIndex, (taxIndex) => taxLabels[taxIndex]);
+
+const priceWithTax = derived([price, tax], ([price, tax]) => price * tax);
+
+export const volumes = [30, 50, 75, 90, 100, 125, 150, 200, 500];
+export const volumeLabels = ['30ml', '50ml', '75ml', '90ml', '100ml', '125ml', '150ml', '200ml', '500ml'];
+export const volumeIndex = writable(4);
+export const volume = derived(volumeIndex, from(volumes));
+export const volumeLabel = derived(volumeIndex, from(volumeLabels));
+
+export const sizes = [0.25, 0.5, 1, 1.5, 2];
+export const sizeLabels = ['XS', 'S', 'M', 'L', 'XL'];
+export const sizeIndex = writable(2);
+export const size = derived(sizeIndex, from(sizes));
+export const sizeLabel = derived(sizeIndex, from(sizeLabels));
+
+export const weight = writable(4); // oz
+
+const shippingWeight = derived([category, volume, size, weight], ([category, volume, size, weight]) => {
+  switch (category) {
+    case 'perfume':
+      return volume / 100; // 100 ml bottle is typically 1 lb
+    case 'cosmetics':
+      return size;
+    case 'lipsticks':
+      return lipstickWeight;
+    default:
+      return weight / 16;
+  }
+}); // lb
+const shipping = derived(shippingWeight, (shippingWeight) => Math.ceil(shippingCost * shippingWeight));
+
+const custom = derived([category, priceWithTax, shippingWeight], ([category, priceWithTax, shippingWeight]) => {
+  switch (category) {
+    case 'perfume':
+      return perfumeCustom;
+    case 'cosmetics':
+      return cosmeticsCustom * shippingWeight;
+    case 'lipsticks':
+      return lipstickCustom(priceWithTax);
+    default:
+      return 0;
+  }
+});
+
+export const finalUsd = derived([priceWithTax, shipping, custom], ([priceWithTax, shipping, custom]) => priceWithTax + shipping + custom);
+export const finalVnd = derived(finalUsd, (finalUsd) => Math.ceil(finalUsd * usdToVnd / 50000) * 50000);
+export const final = derived(finalVnd, friendlyVND);
+
+function friendlyVND(value: number) {
+  const text = value + '';
+
+  const tokens = [];
+  let i;
+  for (i = text.length - 3; i > 0; i -= 3) {
+    tokens.unshift(text.substr(i, 3));
+  }
+  tokens.unshift(text.substring(0, i + 3));
+  return `${tokens.join(',')} VND`;
+}
+
+function from(array: any[]) {
+  return (index: number) => array[index];
+}
