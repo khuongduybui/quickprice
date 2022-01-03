de29c67848c93bacbd7e4265ac6c32a651635b55
Duy K. Bui <duy@buifamily.info>
2022-01-02 20:16:56 -0500

    feat: new perfume

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,167 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+  import {
+    categoryIndex,
+    categoryLabels,
+    price,
+    taxes,
+    taxIndex,
+    taxLabel,
+    volumes,
+    volumeIndex,
+    volumeLabel,
+    sizes,
+    sizeIndex,
+    sizeLabel,
+    weight,
+    final,
+    fullHistory,
+    lipstickWeight,
+  } from "../data";
 </script>
 
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
+<page>
+  <actionBar title="Tính Giá Nhanh" />
+  <dockLayout stretchLastChild="true">
+    <stackLayout dock="top">
+      <!--
+      <segmentedBar bind:selectedIndex={$categoryIndex}>
+        {#each categoryLabels as text}
+          <segmentedBarItem title={text} />
+        {/each}
+      </segmentedBar>
+      <stackLayout class="hr" />
+      -->
+      <gridLayout rows="auto auto auto" columns="60 * 75">
+        <label row="0" col="0" text="Giá" />
+        <textField row="0" col="1" bind:text={$price} keyboardType="number" />
+        <label row="0" col="2" text="USD" />
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
+        <label row="1" col="0" text="Thuế" />
+        <slider
+          row="1"
+          col="1"
+          bind:value={$taxIndex}
+          minValue="0"
+          maxValue={taxes.length - 1}
+        />
+        <label row="1" col="2" text={$taxLabel} textWrap="true" />
+
+        <label row="2" col="0" text="Cỡ" />
+        <!-- Perfume -->
+        <slider
+          row="2"
+          col="1"
+          bind:value={$volumeIndex}
+          minValue="0"
+          maxValue={volumes.length - 1}
+          visibility={$categoryIndex === 0 ? "visible" : "collapsed"}
+        />
+        <label
+          row="2"
+          col="2"
+          text={$volumeLabel}
+          visibility={$categoryIndex === 0 ? "visible" : "collapsed"}
+          textWrap="true"
+        />
+        <!-- Cosmetics -->
+        <slider
+          row="2"
+          col="1"
+          bind:value={$sizeIndex}
+          minValue="0"
+          visibility={$categoryIndex === 1 ? "visible" : "collapsed"}
+          maxValue={sizes.length - 1}
+        />
+        <label
+          row="2"
+          col="2"
+          text={$sizeLabel}
+          visibility={$categoryIndex === 1 ? "visible" : "collapsed"}
+          textWrap="true"
+        />
+        <!-- Lipsticks -->
+        <label
+          row="2"
+          col="1"
+          text="4"
+          class="textField"
+          visibility={$categoryIndex === 2 ? "visible" : "collapsed"}
+        />
+        <label
+          row="2"
+          col="2"
+          text="oz"
+          visibility={$categoryIndex === 2 ? "visible" : "collapsed"}
+          textWrap="true"
+        />
+        <!-- Others -->
+        <textField
+          row="2"
+          col="1"
+          bind:text={$weight}
+          keyboardType="number"
+          visibility={$categoryIndex > 2 ? "visible" : "collapsed"}
+        />
+        <label
+          row="2"
+          col="2"
+          text="oz"
+          visibility={$categoryIndex > 2 ? "visible" : "collapsed"}
+          textWrap="true"
+        />
+      </gridLayout>
+      <stackLayout class="hr" />
+      <label class="h1 text-center text-primary" text={$final} />
+      <button
+        class="-primary"
+        on:tap={() => {
+          const entry = [
+            $categoryIndex,
+            `$ ${$price}`,
+            $taxLabel,
+            `${$weight} oz`,
+            $final,
+          ];
+          if ($categoryIndex === 0) entry[3] = $volumeLabel;
+          if ($categoryIndex === 1) entry[3] = $sizeLabel;
+          if ($categoryIndex === 2) entry[3] = `${lipstickWeight * 16} oz`;
+          $fullHistory = [entry].concat($fullHistory);
+        }}>Lưu</button
+      >
+      <stackLayout class="hr" />
+    </stackLayout>
+
+    {#if $fullHistory.length}
+      <button dock="bottom" class="-outline" on:tap={() => ($fullHistory = [])}>
+        Xóa sạch
+      </button>
+    {/if}
+
+    <scrollView>
+      <stackLayout>
+        {#each $fullHistory as record, index (record)}
+          {#if record[0] === $categoryIndex}
+            <gridLayout rows="auto" columns="70 60 75 * 90">
+              {#each record as text, col}
+                {#if col > 0}
+                  <label row="0" col={col - 1} {text} />
+                {:else}
+                  <button
+                    row="0"
+                    col="4"
+                    on:tap={() => {
+                      $fullHistory.splice(index, 1);
+                      $fullHistory = $fullHistory;
+                    }}
+                  >
+                    Xóa
+                  </button>
+                {/if}
+              {/each}
+            </gridLayout>
+          {/if}
+        {/each}
+      </stackLayout>
+    </scrollView>
+  </dockLayout>
+</page>
---
+++ app/data.ts
@@ -0,0 +1,101 @@
+import { Unsubscriber, writable, derived } from 'svelte/store';
+import { ApplicationSettings } from '@nativescript/core';
+
+const usdToVnd = 23_500;
+const shippingCost = 5; // $/lb
+export const lipstickWeight = 0.25; // lb
+const perfumeCustom = 6; // $/item
+const cosmeticsCustom = 2.5; // $/lb
+const lipstickCustom = (priceWithTax: number) => {
+  if (priceWithTax >= 30) return 1.5;
+  return 0.5;
+}; // $/item
+
+const from = (array: any[]) => (index: number) => array[index];
+const friendlyVnd = (value: number) => {
+  const text = `${value}`;
+
+  const tokens = [];
+  let i;
+  for (i = text.length - 3; i > 0; i -= 3) {
+    tokens.unshift(text.substr(i, 3));
+  }
+  tokens.unshift(text.substring(0, i + 3));
+  return `${tokens.join(',')} VND`;
+};
+
+export const categories = ['perfume', 'cosmetics', 'lipsticks', 'others'];
+export const categoryLabels = ['Nước hoa', 'Mỹ phẩm', 'Son', 'Khác'];
+export const categoryIndex = writable(0);
+export const category = derived(categoryIndex, from(categories));
+export const categoryLabel = derived(categoryIndex, from(categoryLabels));
+
+export const price = writable('100');
+
+export const taxes = [1, 1.05, 1.06, 1.075, 1.1];
+export const taxLabels = ['0%', '5%', '6%', '7.5%', '10%'];
+export const taxIndex = writable(2);
+export const tax = derived(taxIndex, from(taxes));
+export const taxLabel = derived(taxIndex, from(taxLabels));
+
+const priceWithTax = derived([price, tax], ([price, tax]) => Number(price) * tax);
+
+export const volumes = [30, 50, 75, 90, 100, 125, 150, 200, 500];
+export const volumeLabels = ['30ml', '50ml', '75ml', '90ml', '100ml', '125ml', '150ml', '200ml', '500ml'];
+export const volumeIndex = writable(4);
+export const volume = derived(volumeIndex, from(volumes));
+export const volumeLabel = derived(volumeIndex, from(volumeLabels));
+
+export const sizes = [0.25, 0.5, 1, 1.5, 2];
+export const sizeLabels = ['XS', 'S', 'M', 'L', 'XL'];
+export const sizeIndex = writable(2);
+export const size = derived(sizeIndex, from(sizes));
+export const sizeLabel = derived(sizeIndex, from(sizeLabels));
+
+export const weight = writable('16'); // oz
+
+const shippingWeight = derived([category, volume, size, weight], ([category, volume, size, weight]) => {
+  switch (category) {
+    case 'perfume':
+      return volume / 100; // 100 ml bottle is typically 1 lb
+    case 'cosmetics':
+      return size;
+    case 'lipsticks':
+      return lipstickWeight;
+    default:
+      return Number(weight) / 16; // weight in oz
+  }
+}); // lb
+const shipping = derived(shippingWeight, (shippingWeight) => Math.ceil(shippingCost * shippingWeight));
+
+const custom = derived([category, priceWithTax, shippingWeight], ([category, priceWithTax, shippingWeight]) => {
+  switch (category) {
+    case 'perfume':
+      return perfumeCustom;
+    case 'cosmetics':
+      return cosmeticsCustom * shippingWeight;
+    case 'lipsticks':
+      return lipstickCustom(priceWithTax);
+    default:
+      return 0;
+  }
+});
+
+export const finalUsd = derived([priceWithTax, shipping, custom], ([priceWithTax, shipping, custom]) => priceWithTax + shipping + custom);
+export const finalVnd = derived(finalUsd, (finalUsd) => Math.ceil((finalUsd * usdToVnd) / 50_000) * 50_000);
+export const final = derived(finalVnd, friendlyVnd);
+
+export const fullHistory = writable([], (set) => {
+  if (ApplicationSettings.hasKey('fullHistory')) {
+    const storedBlob = ApplicationSettings.getString('fullHistory');
+    try {
+      const result = JSON.parse(storedBlob);
+      set(result);
+    } catch (_) {}
+  }
+  const unsubscribe: Unsubscriber = fullHistory.subscribe((history) => {
+    const blobToStore = JSON.stringify(history);
+    ApplicationSettings.setString('fullHistory', blobToStore);
+  });
+  return () => unsubscribe();
+});
