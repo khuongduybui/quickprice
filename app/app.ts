53a2947927c8e3329818329c0b2e3583fdc9e4dd
Duy K. Bui <duy@buifamily.info>
2022-08-14 11:07:17 -0400

    lint: trunk fmt

--- app/app.ts
+++ app/app.ts
@@ -4,7 +4,7 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first page.
 */
 
-import { svelteNativeNoFrame } from 'svelte-native'
-import App from './App.svelte'
+import { svelteNativeNoFrame } from 'svelte-native';
+import App from './App.svelte';
 
-svelteNativeNoFrame(App, {})
+svelteNativeNoFrame(App, {});
